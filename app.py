49655961bbb9c1e847a4aee25cc17d6c277631cd
Khevana-pat <patelkhevana18@gmail.com>
2023-04-20 15:03:21 -0400

    add config.py&cleandata_analysis.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, render_template
+from sqlalchemy import create_engine
+from config import db_user, db_password, db_host, db_port, db_name
+
+
+app = Flask(__name__)
+engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api/language.json")
+def summary():
+    results = engine.execute("SELECT * FROM programming_language")
+    return jsonify([dict(_) for _ in results])
+
+@app.route("/api/visual1.json")
+def summary():
+    results = engine.execute("SELECT * FROM programming_language")
+    return jsonify([dict(_) for _ in results])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+
+
